720b8e5e3f1636a643e08da988151075dcbf1e79
alhayat-dev <mudasser@royalbathrooms.co.uk>
2021-02-16 22:31:43 +0500

    Create and use observers in Magento 2

---
+++ ObserverExample/Logger/Logger.php
@@ -0,0 +1,12 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Training\ObserverExample\Logger;
+
+
+class Logger extends \Monolog\Logger
+{
+
+}
---
+++ ObserverExample/Observer/Frontend/Example.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\ObserverExample\Observer\Frontend;
+
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Training\ObserverExample\Logger\Logger;
+
+class Example implements ObserverInterface
+{
+    /**
+     * @var Logger
+     */
+    private Logger $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $this->logger->info('Event Triggered in frontend scope');
+    }
+}
