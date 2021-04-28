9681dd983c0aa0f3c3871be1c7f70fb270f102ae
Mudasser Hayat <mudasser@royalbathrooms.co.uk>
2021-04-28 05:25:41 +0500

    Ifconfig usage and hide block using system configuration.

---
+++ IfConfigExample/Controller/Index/Index.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Training\IfConfigExample\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
